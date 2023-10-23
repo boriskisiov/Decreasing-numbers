ddabb440251ea63f634ca76b2dfdab10fc238160
boriskisiov <boriskisiov@gmail.com>
2023-10-23 15:59:25 +0300

    Add project files.

---
+++ Decreasing numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Decreasing_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            while (number >= 1) {
+            Console.WriteLine(number);
+                number--;
+            }
+        }
+    }
+}
